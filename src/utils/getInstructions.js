1c83d66f28a480421f5f33c29aaba3784418aba6
tsrrodrigues <tsrrodrigues@outlook.com>
2021-02-21 00:13:39 -0300

    Adicionando o retorno das mensagens de intrução ao algoritmo de menor rota.
    Co-authored-by: Ítalo Alves <italo.guimaraes@aluno.unb.br>

--- src/utils/getInstructions.js
+++ src/utils/getInstructions.js
@@ -28,19 +28,25 @@ const BFS = (startStation, endStation) => {
       }
     })
   }
-
   return false
 }
 
 const getInstructions = (startStation, endStation) => {
   BFS(startStation, endStation)
+  let path = []
   let parent = endStation.id
   while (parent !== startStation.id) {
-    console.log(stationsGraph[parent].stationName)
+    path.unshift(stationsGraph[parent])
     parent = parents[parent]
   }
-  console.log(stationsGraph[parent].stationName)
+  path.unshift(stationsGraph[parent])
+  let mensagens = []
+  mensagens.push(`Embarque em ${path[0].stationName} sentido à estação ${path[1].stationName}`)
+  for (let i = 1; i < path.length - 1; i += 1) {
+    if (path[i].lineName === 'Estação de Integração') {
+      mensagens.push(`Na estação ${path[i].stationName} siga sentido à estação ${path[i + 1].stationName}`)
+    }
+  }
+  mensagens.push(`Desembarque em ${path[path.length - 1].stationName}`)
+  return mensagens
 }
-
-getInstructions(stationsGraph[0], stationsGraph[104])
-
